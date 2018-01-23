34e43e10a0bd2932596530766547a933c260808d
Leo Poulson <leopoulson@gmail.com>
2018-01-23 13:52:50 +0000

    added rule for and

--- Main.hs
+++ Main.hs
@@ -3,7 +3,7 @@ data Tree a = Node a (Tree a) (Tree a) | Leaf deriving Show
 instance Functor Tree where
     fmap f (Node a t1 t2) = Node (f a) (fmap f t1) (fmap f t2)
     fmap f Leaf = Leaf
-    
+
 
 data Form = Var Char | Not Form | And Form Form | Or Form Form
     deriving Show
@@ -20,6 +20,8 @@ data Form = Var Char | Not Form | And Form Form | Or Form Form
 growTree :: Tree [Form] -> Tree [Form]
 growTree Leaf = Leaf
 growTree (Node (f:fs) Leaf Leaf) = case f of 
+    And f1 f2    -> (Node (f1 : f2 : fs) Leaf Leaf)
+    Or f1 f2     -> (Node fs (Node (f1:fs) Leaf Leaf) (Node (f2:fs) Leaf Leaf))
     Not (Not f') -> (Node (f':fs) Leaf Leaf)
 
 doubleNeg :: Form -> Form
