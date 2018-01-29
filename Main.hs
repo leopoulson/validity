867df0da1e7b6d6e6607349f224e60dcc564232c
Leo Poulson <lp16860@my.bristol.ac.uk>
2018-01-29 13:42:57 +0000

    implemented zippers. actually have something that looks like it could be used

--- Main.hs
+++ Main.hs
@@ -1,4 +1,8 @@
+import Zipper
+
 data Tree a = Node a (Tree a) (Tree a) | Leaf deriving Show
+
+type ZTree a = Tree (Zipper a)
 
 instance Functor Tree where
     fmap f (Node a t1 t2) = Node (f a) (fmap f t1) (fmap f t2)
@@ -13,11 +17,27 @@ growTree :: Tree [Form] -> Tree [Form]
 growTree Leaf = Leaf
 growTree (Node (f:fs) b1 b2) = case f of 
     And f1 f2       -> (Node (f1 : f2 : fs) b1 b2)
-    Or f1 f2        -> (Node fs (growTree (addToTree b1 f1)) (growTree (addToTree b2 f2)))
-    Var c           -> (Node [(Var c)] b1 b2)
     Not (Or f1 f2)  -> (Node ((Not f1) : (Not f2) : fs) b1 b2)
-    Not (And f1 f2) -> (Node fs (addToTree b1 (Not f1)) (addToTree b2 (Not f2)))
+    Var c           -> (Node [(Var c)] b1 b2)
     Not (Not f')    -> (Node (f':fs) b1 b2)
+    _               -> doubleRule (Node (f:fs) b1 b2) f
+    -- Or f1 f2        -> (Node fs (growTree (addToTree b1 f1)) (growTree (addToTree b2 f2)))
+    -- Not (And f1 f2) -> (Node fs (addToTree b1 (Not f1)) (addToTree b2 (Not f2)))
+    
+zipifyTree :: [Form] -> ZTree Form
+zipifyTree [] = Leaf
+zipifyTree xs = Node ((fromList) xs) Leaf Leaf
+
+growZTree :: ZTree Form -> ZTree Form
+growZTree Leaf = Leaf
+growZTree (Node z bl br) 
+    | endp z    = Node z (growZTree bl) (growZTree br)
+    | otherwise = (growZTree . applyZRule) (Node z bl br)
+
+applyZRule :: ZTree Form -> ZTree Form
+applyZRule (Node z bl br) = case cursor z of
+    Var c     -> Node (right z) bl br
+    And f1 f2 -> Node (insert f1 (insert f2 (right z))) bl br
 
 
 --This will add the given Form to the list of Forms in the point at the tree.
@@ -27,7 +47,8 @@ addToTree (Node fs t1 t2) f = Node (f:fs) t1 t2
 
 doubleRule :: Tree [Form] -> Form -> Tree [Form]
 doubleRule (Node fs Leaf Leaf) f = case f of
-    Or f1 f2 -> (Node fs (Node [f1] Leaf Leaf) (Node [f2] Leaf Leaf))
+    Or f1 f2        -> (Node fs (Node [f1] Leaf Leaf) (Node [f2] Leaf Leaf))
+    Not (And f1 f2) -> (Node fs (Node [Not f1] Leaf Leaf) (Node [Not f2] Leaf Leaf))
 doubleRule (Node fs b1 b2) f = Node fs (doubleRule b1 f) (doubleRule b2 f)
 
 doubleNeg :: Form -> Form
