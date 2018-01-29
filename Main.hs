38a1c180285cf5dcfa4eb67c761a055793871f3d
Leo Poulson <lp16860@my.bristol.ac.uk>
2018-01-29 13:04:19 +0000

    added new rule for making two branches

--- Main.hs
+++ Main.hs
@@ -8,27 +8,27 @@ instance Functor Tree where
 data Form = Var Char | Not Form | And Form Form | Or Form Form
     deriving Show
 
-
-
--- validTree :: Tree Bool -> Bool
--- validTree Leaf = True
--- validTree (Node True t1 t2) = (validTree t1) && (validTree t2)
--- validTree (Node False t1 t2) = False
-
 --so the tree keeps track of the rules that it has left to apply
 growTree :: Tree [Form] -> Tree [Form]
 growTree Leaf = Leaf
 growTree (Node (f:fs) b1 b2) = case f of 
     And f1 f2       -> (Node (f1 : f2 : fs) b1 b2)
-    Or f1 f2        -> (Node fs (addToTree b1 f1) (addToTree b2 f2))
+    Or f1 f2        -> (Node fs (growTree (addToTree b1 f1)) (growTree (addToTree b2 f2)))
+    Var c           -> (Node [(Var c)] b1 b2)
     Not (Or f1 f2)  -> (Node ((Not f1) : (Not f2) : fs) b1 b2)
     Not (And f1 f2) -> (Node fs (addToTree b1 (Not f1)) (addToTree b2 (Not f2)))
     Not (Not f')    -> (Node (f':fs) b1 b2)
+
 
 --This will add the given Form to the list of Forms in the point at the tree.
 addToTree :: Tree [Form] -> Form -> Tree [Form]
 addToTree Leaf f = Node [f] Leaf Leaf
 addToTree (Node fs t1 t2) f = Node (f:fs) t1 t2
+
+doubleRule :: Tree [Form] -> Form -> Tree [Form]
+doubleRule (Node fs Leaf Leaf) f = case f of
+    Or f1 f2 -> (Node fs (Node [f1] Leaf Leaf) (Node [f2] Leaf Leaf))
+doubleRule (Node fs b1 b2) f = Node fs (doubleRule b1 f) (doubleRule b2 f)
 
 doubleNeg :: Form -> Form
 doubleNeg (Not (Not f)) = f
